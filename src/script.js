32819ccd100d70b3003a60f72048bf2a49b0c98c
Jibe_Disko <jean-baptiste.giffard@disko.fr>
2023-01-11 20:58:42 +0100

    big update

--- src/script.js
+++ src/script.js
@@ -1,7 +1,13 @@
+
+import './css/webflow.css'
+import './css/normalize.css'
+import './css/arthurh.webflow.css'
 import './style.css'
+
+
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
 
 // Tools
 const lerp = (x, y, a) => x * (1 - a) + y * a;
@@ -19,7 +25,7 @@ const scene = new THREE.Scene()
 /**
  * Sizes
  */
- const sizes = {
+var sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
@@ -36,16 +42,55 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true,
+    antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(window.devicePixelRatio);
+
+window.addEventListener('resize', onWindowResize);
+
+function onWindowResize() {
+  sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+  }
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+
+
+}
 
 
 /**
  * Orbit controls
  */
-const controls = new OrbitControls( camera, renderer.domElement );
-controls.enableDamping = true;
+const controls = new OrbitControls(camera, canvas);
+
+controls.enableDamping = true
+controls.enableRotate = false
+
+
+//controls.enableZoom=false;
+//controls.enablePan=false;
+
+
+/*
+controls.minDistance = 0.6
+controls.maxDistance = 30
+*/
+
+
+controls.mouseButtons = {
+    LEFT: THREE.MOUSE.PAN,
+    MIDDLE: THREE.MOUSE.DOLLY,
+    RIGHT: THREE.MOUSE.PAN
+}
+
+controls.touches.ONE = THREE.TOUCH.PAN;
+controls.touches.TWO = THREE.TOUCH.DOLLY_ROTATE;
 
 
 /**
@@ -55,17 +100,17 @@ controls.enableDamping = true;
 const loadingManager = new THREE.LoadingManager(
     // Loaded
     () => {
-        requestAnimationFrame( animate );
-
+        requestAnimationFrame(animate);
+        document.querySelector('.section-intro').classList.remove('hide')
     },
-  
+
     // Progress
     (itemUrl, itemsLoaded, itemsTotal) => {
-     const progressRatio = itemsLoaded / itemsTotal
-     console.log(itemUrl, itemsLoaded, itemsTotal)
+        const progressRatio = itemsLoaded / itemsTotal
+        //console.log(itemUrl, itemsLoaded, itemsTotal)
 
     }
-   )
+)
 
 
 
@@ -80,20 +125,72 @@ const goldTexture = textureLoader.load('textures/gold.jpg')
  */
 const gold = new THREE.MeshMatcapMaterial({ matcap: goldTexture });
 
+const video = document.querySelector('.main-video');
+const videoTexture = new THREE.VideoTexture(video)
+videoTexture.magFilter = THREE.LinearFilter;
+
+const material = new THREE.MeshBasicMaterial({ map: videoTexture });
+
 /**
  * Objects
  */
- const geometry = new THREE.BoxGeometry(1, 1, 1)
- const mesh = new THREE.Mesh(geometry, gold)
- scene.add(mesh)
- 
+const geometry = new THREE.PlaneGeometry(3, 3)
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+//mesh.position.y=.59
+
+mesh.visible = false;
 
 /**
  * Animate
  */
+
 function animate() {
-	requestAnimationFrame( animate );
-	controls.update();
-	renderer.render( scene, camera );
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
 }
 
+
+/**
+ * UI
+ */
+
+
+document.querySelector('.cta-intro').addEventListener('click',()=>{
+    document.querySelector('.section-intro').classList.add('hide')
+
+    setTimeout(()=>{
+        document.querySelector('.section-tuto').classList.remove('hide')
+        document.querySelector('.logo-title').classList.remove('hide')
+
+    },500)
+})
+
+
+
+document.querySelector('.cta-tuto').addEventListener('click',()=>{
+    document.querySelector('.section-tuto').classList.add('hide')
+
+    setTimeout(()=>{
+        document.querySelector('.order-cta').classList.remove('hide')
+        document.querySelector('.main-canvas').classList.remove('hide')
+        document.body.classList.add('grab')
+
+
+    },500)
+
+    mesh.visible = true;
+})
+
+
+
+document.body.addEventListener('mousedown',()=>{
+    if(document.body.classList.contains('grab')){
+        document.body.classList.add('grabbing')
+    }
+})
+
+document.body.addEventListener('mouseup',()=>{
+        document.body.classList.remove('grabbing')
+})
