9d21e842c6da5448f14ace6ec289485a5453c262
Jibe_Disko <jean-baptiste.giffard@disko.fr>
2023-01-12 15:10:40 +0100

    update

--- src/script.js
+++ src/script.js
@@ -19,7 +19,7 @@ const range = (x1, y1, x2, y2, a) => lerp(x2, y2, invlerp(x1, y1, a));
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-const legend= document.querySelector('.legend')
+const legend = document.querySelector('.legend')
 
 // Scene
 const scene = new THREE.Scene()
@@ -54,13 +54,13 @@ renderer.setPixelRatio(window.devicePixelRatio);
 window.addEventListener('resize', onWindowResize);
 
 function onWindowResize() {
-  sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-  }
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-  renderer.setSize(sizes.width, sizes.height);
+    sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width, sizes.height);
 
 
 }
@@ -147,19 +147,19 @@ mesh.visible = false;
  * Animate
  */
 
-let tracks=[
-    {name:"La vie",x:100,y:110,element:'.p0',content:`“J’ai écouté Léo ferré et j’ai écrit cette chanson d’un jet, en deux heures, sans douter. Toujours dans l’idée de l’ombre et de la lumière, Eros et Thanatos, la vie comme voyage initiatique…”`},
-    {name:"La route",x:48,y:-7,element:'.p1',content:`“Un ami proche a accompagné sa femme trois ans dans une lutte âpre contre un cancer grave. Elle a gagné la bataille. J’ai demandé à mon ami qu’est-ce qu’il retirait de cette expérience si intense. Il m’a répondu : maintenant quand j’ai un rêve je le vis, je n’attends plus, je ne le reporte pas.”  `},  
-    {name:"L'ocean",x:-10,y:-26,element:'.p2',content:`“Marcher sur les immenses plages désertes des Landes et penser à ceux qu’on aime.”`},
-    {name:"Le secret",x:-100,y:-10,element:'.p3',content:`“Une des nombreuses histoires de la vie de mon père. Une histoire d’abus qui a duré longtemps. Tout doit être dit, tout doit être su, tout se saura…” `},
-    {name:"El magnifico",x:-90,y:-90,element:'.p4',content:`“La vision d’un oiseau de feu. Un conte russe que j’adorais quand j’étais enfant. Une histoire d’amitié dans la tourmente.”`},
-    {name:"Titanic",x:10,y:-60,element:'.p5',content:`“Une parabole poético-politique. Dans l’orchestre du Titanic, il y avait un musicien français qui a dû continuer à jouer jusqu’au bout, la musique empêchait, apparemment, les gens de paniquer. Je me suis imaginé à sa place, jouant pour les étoiles et la nuit et la vie.”`},
-    {name:"Divin blaspheme",x:-115,y:40,element:'.p6',content:`“Une chanson déclaration hommage pour ma reine Brigitte Fontaine. Ma petite fée bizarre qui était là à ma naissance et qui ne m’a jamais quitté. Une artiste fantastique et une source continue d’inspiration : audace, poésie, humour, créativité débridée… “ `},
-    {name:"L innocence",x:-65,y:-30,element:'.p7',content:`“Une chanson dramatique et excitante de crise de couple ou, peut-être, chacun pourra se reconnaître.” `},
-    {name:"Addict",x:65,y:-90,element:'.p8',content:`“Nous vivons dans une société basée sur l’addiction, à tous les niveaux, à toutes les échelles, l’addiction comme façon de vivre, de consommer, de rêver. L’addiction non pas tolérée mais encouragée dans des proportions délirantes, une société saine ?”`},
-    {name:"La folie du controle",x:130,y:70,element:'.p9',content:`“Petite réflexion philosophique sur la pulsions archaïque et paranoïaque des gens de pouvoir pour le contrôle et la surveillance, sous toutes ses formes. Sur la disparition programmée de contre-pouvoirs efficaces. Inspiré de faits réels et récents.”  `},
-    {name:"Cet amour me tue",x:120,y:-50,element:'.p10',content:`“Cette chanson est dans le film « Sound of Metal » de Darius Marder où Mathieu Amalric et Olivia Cooke la fredonne. Je l’ai ensuite chanté avec Martha Wainwright pour la BO du film. J’ai refait une version pour le disque avec des chœurs de Léonore et des cordes-samples très sombres de Nicolas Repac.” `},
-    {name:"L etoile",x:-120,y:-130,element:'.p11',content:`“Une comptine cosmique et une chanson d’amour pour ma femme. Avec la harpe merveilleuse de Pauline Haaset les matières sonores si riches de Thomas Bloch aux Ondes Martenots et au Cristal Baschet.”`},
+let tracks = [
+    { name: "La vie", x: 100, y: 110, element: '.p0', content: `“J’ai écouté Léo ferré et j’ai écrit cette chanson d’un jet, en deux heures, sans douter. Toujours dans l’idée de l’ombre et de la lumière, Eros et Thanatos, la vie comme voyage initiatique…”` },
+    { name: "La route", x: 48, y: -7, element: '.p1', content: `“Un ami proche a accompagné sa femme trois ans dans une lutte âpre contre un cancer grave. Elle a gagné la bataille. J’ai demandé à mon ami qu’est-ce qu’il retirait de cette expérience si intense. Il m’a répondu : maintenant quand j’ai un rêve je le vis, je n’attends plus, je ne le reporte pas.”  ` },
+    { name: "L'ocean", x: -10, y: -26, element: '.p2', content: `“Marcher sur les immenses plages désertes des Landes et penser à ceux qu’on aime.”` },
+    { name: "Le secret", x: -100, y: -10, element: '.p3', content: `“Une des nombreuses histoires de la vie de mon père. Une histoire d’abus qui a duré longtemps. Tout doit être dit, tout doit être su, tout se saura…” ` },
+    { name: "El magnifico", x: -90, y: -90, element: '.p4', content: `“La vision d’un oiseau de feu. Un conte russe que j’adorais quand j’étais enfant. Une histoire d’amitié dans la tourmente.”` },
+    { name: "Titanic", x: 10, y: -60, element: '.p5', content: `“Une parabole poético-politique. Dans l’orchestre du Titanic, il y avait un musicien français qui a dû continuer à jouer jusqu’au bout, la musique empêchait, apparemment, les gens de paniquer. Je me suis imaginé à sa place, jouant pour les étoiles et la nuit et la vie.”` },
+    { name: "Divin blaspheme", x: -115, y: 40, element: '.p6', content: `“Une chanson déclaration hommage pour ma reine Brigitte Fontaine. Ma petite fée bizarre qui était là à ma naissance et qui ne m’a jamais quitté. Une artiste fantastique et une source continue d’inspiration : audace, poésie, humour, créativité débridée… “ ` },
+    { name: "L innocence", x: -65, y: -30, element: '.p7', content: `“Une chanson dramatique et excitante de crise de couple ou, peut-être, chacun pourra se reconnaître.” ` },
+    { name: "Addict", x: 65, y: -90, element: '.p8', content: `“Nous vivons dans une société basée sur l’addiction, à tous les niveaux, à toutes les échelles, l’addiction comme façon de vivre, de consommer, de rêver. L’addiction non pas tolérée mais encouragée dans des proportions délirantes, une société saine ?”` },
+    { name: "La folie du controle", x: 130, y: 70, element: '.p9', content: `“Petite réflexion philosophique sur la pulsions archaïque et paranoïaque des gens de pouvoir pour le contrôle et la surveillance, sous toutes ses formes. Sur la disparition programmée de contre-pouvoirs efficaces. Inspiré de faits réels et récents.”  ` },
+    { name: "Cet amour me tue", x: 120, y: -50, element: '.p10', content: `“Cette chanson est dans le film « Sound of Metal » de Darius Marder où Mathieu Amalric et Olivia Cooke la fredonne. Je l’ai ensuite chanté avec Martha Wainwright pour la BO du film. J’ai refait une version pour le disque avec des chœurs de Léonore et des cordes-samples très sombres de Nicolas Repac.” ` },
+    { name: "L etoile", x: -120, y: -130, element: '.p11', content: `“Une comptine cosmique et une chanson d’amour pour ma femme. Avec la harpe merveilleuse de Pauline Haaset les matières sonores si riches de Thomas Bloch aux Ondes Martenots et au Cristal Baschet.”` },
 ]
 
 let points = [];
@@ -168,12 +168,12 @@ const geo = new THREE.SphereGeometry(.025, 32, 16);
 const mat = new THREE.MeshBasicMaterial({ color: 0xffff00, transparent: true, opacity: 0 });
 
 
-tracks.forEach((track,index)=>{
-    points.push(new THREE.Mesh(geo,mat));
-    points[index].element=document.querySelector(track.element)
+tracks.forEach((track, index) => {
+    points.push(new THREE.Mesh(geo, mat));
+    points[index].element = document.querySelector(track.element)
     scene.add(points[index]);
-    points[index].position.x=track.x/100
-    points[index].position.y=-track.y/100
+    points[index].position.x = track.x / 100
+    points[index].position.y = -track.y / 100
 })
 
 
@@ -191,7 +191,7 @@ function animate() {
         const translateY = -screenPosition.y * sizes.height * 0.5 - 11.5
 
         point.element.style.transform = `translate(${translateX}px, ${translateY}px)`
-      }
+    }
 }
 
 
@@ -200,83 +200,93 @@ function animate() {
  */
 
 
-document.querySelector('.cta-intro').addEventListener('click',()=>{
+document.querySelector('.cta-intro').addEventListener('click', () => {
     document.querySelector('.section-intro').classList.add('hide')
 
-    setTimeout(()=>{
+    setTimeout(() => {
         document.querySelector('.section-tuto').classList.remove('hide')
         document.querySelector('.logo-title').classList.remove('hide')
-    },500)
+    }, 500)
 })
 
 
 
-document.querySelector('.cta-tuto').addEventListener('click',()=>{
+
+
+const openCover = () => {
     document.querySelector('.section-tuto').classList.add('hide')
 
-    setTimeout(()=>{
+    setTimeout(() => {
         document.querySelector('.order-cta').classList.remove('hide')
         document.querySelector('.main-canvas').classList.remove('hide')
         document.body.classList.add('grab')
         document.querySelector('.points').classList.remove('hide')
 
-    },500)
+    }, 500)
 
     mesh.visible = true;
+}
+
+document.querySelector('.cta-tuto').addEventListener('click',()=>{
+    openCover();
+})
+
+document.querySelector('.lavie').addEventListener('click',()=>{
+    openCover();
 })
 
 
 
-document.body.addEventListener('mousedown',()=>{
-    if(document.body.classList.contains('grab')){
+document.body.addEventListener('mousedown', () => {
+    if (document.body.classList.contains('grab')) {
         document.body.classList.add('grabbing')
     }
 })
 
-document.body.addEventListener('mouseup',()=>{
-        document.body.classList.remove('grabbing')
+document.body.addEventListener('mouseup', () => {
+    document.body.classList.remove('grabbing')
 })
 
-document.querySelectorAll('.point').forEach(point=>{
+document.querySelectorAll('.point').forEach(point => {
 
-    point.addEventListener('click',()=>{
-        var num=point.dataset.number
+    point.addEventListener('click', () => {
+        var num = point.dataset.number
 
-        document.querySelector('.player-title').innerHTML=tracks[num].name
-        document.querySelector('.player-text').innerHTML=tracks[num].content
+        document.querySelector('.player-title').innerHTML = tracks[num].name
+        document.querySelector('.player-text').innerHTML = tracks[num].content
 
         document.querySelector('.section-player').classList.remove('hide')
     })
 
-    point.addEventListener('mouseover',()=>{
-        var num=point.dataset.number
-        document.querySelector('.point-text').innerHTML=tracks[num].name
+    point.addEventListener('mouseover', () => {
+        var num = point.dataset.number
+        document.querySelector('.point-text').innerHTML = tracks[num].name
         legend.classList.remove('hide')
     })
 
-    point.addEventListener('mouseleave',()=>{
-       legend.classList.add('hide')
+    point.addEventListener('mouseleave', () => {
+        legend.classList.add('hide')
     })
 
 })
 
 
 
-document.querySelector('.section-player').addEventListener('click',()=>{
+document.querySelector('.section-player').addEventListener('click', () => {
     document.querySelector('.section-player').classList.add('hide')
 })
 
 const mouse = new THREE.Vector2()
 
-document.body.addEventListener('mousemove',(e)=>{
-    var iks =  e.clientX 
-    var igrek =  e.clientY 
+document.body.addEventListener('mousemove', (e) => {
+    var iks = e.clientX
+    var igrek = e.clientY
 
     mouse.x = iks / sizes.width * 2 - 1
     mouse.y = -(igrek / sizes.height) * 2 + 1
 
     var translateX = mouse.x * sizes.width * 0.5
-    var translateY = -mouse.y * sizes.height * 0.5 
+    var translateY = -mouse.y * sizes.height * 0.5
 
     legend.style.transform = `translateX(${translateX}px) translateY(${translateY}px)`
 })
